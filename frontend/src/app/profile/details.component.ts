2d457b76d8f22103f3d17ce7d6c50687820a1ba4
Rimar <rimar12311231@gmail.com>
2025-04-13 11:37:34 +0800

    Refactor home and profile components for improved readability and consistency; relocate environment files to src folder

--- frontend/src/app/profile/details.component.ts
+++ frontend/src/app/profile/details.component.ts
@@ -1,15 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { AccountService } from '@app/_services';
+import { AccountService } from "@app/_services";
 
-@Component({ templateUrl: 'details.component.html' })
+@Component({ templateUrl: "details.component.html" })
 export class DetailsComponent {
-    account: any;
+  account: this.accountService.accountValue;
 
-    constructor(private accountService: AccountService) {
-        this.account = this.accountService.accountValue;
-    }
-
-
+  constructor(private accountService: AccountService) {}
 }
-
